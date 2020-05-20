c32d1ecd27d6ca68bf3f3b93e01d5eabef419ef8
Ziyu Chen <xiaosuyu1997@gmail.com>
2020-05-20 17:26:44 +0800

    feat: Add banner in home page
      Initial banner with simple css;

---
+++ front-end/the-one-truth/src/components/Banner.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="banner">
+    <div id="det_div">
+      <img src="../assets/Home/detective.png" id="det_img"/>
+      <div>The One Truth</div>
+    </div>
+    
+    <slot class="banner_item"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Banner',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+  #banner {
+    background-color: white;
+    box-shadow: 0 7px 5px rgba(32, 32, 32, 0.3);
+    display: flex;
+    justify-content: flex-end;
+    text-align: center;
+  }
+
+  #det_div{
+    height: 50px;
+    flex: 1 0 auto;
+    text-align: left;
+    line-height: 50px;
+    display: flex;
+  }
+
+  #det_img{
+    height: 30px;
+    margin: 10px 20px 10px 20px;
+  }
+
+</style>
---
+++ front-end/the-one-truth/src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="home">
+    <Banner id="banner">
+      <div class="banner_item">剧本</div>
+      <div v-show="logIn" class="banner_item">用户</div>
+      <div v-show="!logIn" class="banner_item">登录</div>
+      <div v-show="!logIn" class="banner_item">注册</div>
+    </Banner>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Banner from '../components/Banner'
+
+export default {
+  name: 'Home',
+  components: {
+    Banner,
+  },
+  data(){
+    return{
+      logIn: false
+    }
+  }
+}
+</script>
+
+<style>
+  #home{
+  }
+
+  .banner_item {
+    margin: 0 10px 0 10px;
+    padding: 0 10px 0 10px;
+    flex: 0 0 auto;
+    height: 50px;
+    text-align: center;
+    line-height: 50px;
+  }
+</style>
